7777a71ffe0cb880ea11c99edf6b066b3175ebc9
Leonel Meque <leonel.meque@glovoapp.com>
2024-12-08 00:47:30 +0100

    refactor: row component
    commit-id:1101818d

--- src/components/row/Row.tsx
+++ src/components/row/Row.tsx
@@ -1,9 +1,12 @@
 import "./Row.css";
+import React from "react";
 
 export type RowProps = {
   children?: React.ReactNode;
 };
 
-export default function Row({ children }: RowProps) {
+export default function Row({children}: RowProps) {
   return <div className="row">{children}</div>;
 }
+
+Row.displayName = 'Row'
