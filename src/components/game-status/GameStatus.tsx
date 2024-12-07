a0bae3c8b33c853c4e0809a14c317ee5c39f521c
Leonel Meque <leonel.meque@glovoapp.com>
2024-12-08 00:47:53 +0100

    refactor: render winner fun outside component
    commit-id:1f3e077a

--- src/components/game-status/GameStatus.tsx
+++ src/components/game-status/GameStatus.tsx
@@ -6,47 +6,46 @@ export type GameStatusProps = {
   gameWinner?: string;
 };
 
+const renderWinner = (gameWinner?: string) => {
+  switch (gameWinner) {
+    case "Draw":
+      return <span>Draw</span>;
+    case "X":
+      return (
+        <img
+          src="/assets/cross.png"
+          alt={`Game winner is ${gameWinner}`}
+          width={24}
+          height={24}
+        />
+      );
+    case "O":
+      return (
+        <img
+          src="/assets/circle.png"
+          alt={`Game winner is ${gameWinner}`}
+          width={24}
+          height={24}
+        />
+      );
+    default:
+      return <span>N/A</span>;
+  }
+};
+
 export function GameStatus({
-  currentMaxMoves,
-  gameWinner,
-  turn,
-}: GameStatusProps) {
-  const renderWinner = () => {
-    const winnerImage =
-      gameWinner === "X" ? "/assets/cross.png" : "/assets/circle.png";
-
-    switch (gameWinner) {
-      case "Draw":
-        return <span>Draw</span>;
-      case "X":
-        return (
-          <img
-            src={winnerImage}
-            alt={`Game winner is ${gameWinner}`}
-            width={24}
-            height={24}
-          />
-        );
-      case "O":
-        return (
-          <img
-            src={winnerImage}
-            alt={`Game winner is ${gameWinner}`}
-            width={24}
-            height={24}
-          />
-        );
-      default:
-        return <span>N/A</span>;
-    }
-  };
-
+                             currentMaxMoves,
+                             gameWinner,
+                             turn,
+                           }: GameStatusProps) {
   return (
     <div className="game-status">
       <h2>Game Status</h2>
       <p aria-label={`Current Player ${turn}`}>Current Player: {turn}</p>
       <p>Current Max Moves (Max is 9): {currentMaxMoves}</p>
-      <div className="game-status__winner">Game Winner: {renderWinner()}</div>
+      <div className="game-status__winner">Game Winner: {renderWinner(gameWinner)}</div>
     </div>
   );
 }
+
+GameStatus.displayName = 'GameStatus'
